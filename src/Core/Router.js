bcc8bac64e8605e5d81c483e770eb1b20615daa9
Eli Tokar <eli.t@pandats.com>
2021-06-20 20:17:38 +0300

    master - better router

--- src/Core/Router.js
+++ src/Core/Router.js
@@ -4,7 +4,7 @@ import Renderer from "./Renderer";
 export default class Router {
     constructor() {
         // var newURL = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname + window.location.search
-        this.url = window.location;
+        this.url = this.getRequestedPath(window.location);
     }
 
     getPathName() {
@@ -19,9 +19,11 @@ export default class Router {
         // Routes.
         switch (pathname) {
             case '/':
+            case 'index.html':
                 document.body.appendChild(Renderer.homeView())
                 break;
             case '/shop':
+            case 'shop':
                 document.querySelector('.hero').remove();
                 document.body.appendChild(Renderer.productsView());
                 Renderer.addLogicToButtons();
@@ -31,5 +33,11 @@ export default class Router {
                 div.innerHTML = `<h1>Not Found</h1> <br /> <p>${pathname}</p>`;
                 return div;
         }
+    }
+
+    getRequestedPath(url) {
+        const splitPath  = url.split('/');
+
+        return splitPath[splitPath.length - 1];
     }
 }
